bf975e8df55234abb97b5e54649fc3d1685c447b
Sneha <snehab@prodigy.net>
2023-04-21 16:24:57 -0500

    Initial Commit

---
+++ githubLesson/githubLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubLessonApp.swift
+//  githubLesson
+//
+//  Created by scholar on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct githubLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
